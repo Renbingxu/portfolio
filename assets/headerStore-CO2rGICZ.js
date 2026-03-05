32f63ea42a9d7df4dfa4620f8815d3b5d9db71d9
Renbingxu <897119822@qq.com>
2026-03-05 21:55:06 +0800

    feat: 修改vite.config

--- assets/headerStore-CO2rGICZ.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index-ChSKnkbc.js";const i=e("splash",{state:()=>({isVisible:!1}),actions:{changeStatus(s){this.isVisible=s}}});export{i as u};
--- assets/personal_photo_02-DCqPOZbg.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/personal_photo_01-By02WNsj.png",o="/assets/personal_photo_02-NLX1WgiO.png";export{s as _,o as a};
